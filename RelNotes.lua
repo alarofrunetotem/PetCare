62449cc850397342ab57ddbbb9721086aa3f2747
Alar of Runetotem <alar@aspide.it>
2024-08-09 20:24:19 +0200

    Added icon

--- RelNotes.lua
+++ RelNotes.lua
@@ -13,7 +13,7 @@ can play a sound if you enter a non PVP instance with Growl active
 alerts can be disabled in PVP
 ]])
 self:HF_Paragraph("Release Notes")
-self:RelNotes(6,10,0,[[
+self:RelNotes(6,10,1,[[
 Toc: 11.0.0, 11.0.2
 ]])
 self:RelNotes(6,9,1,[[
