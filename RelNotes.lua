b50d10f9addbae523c7767efc0d594308d01361b
Alar of Runetotem <alar@aspide.it>
2026-03-07 09:23:37 +0100

    Version 6.15.0

--- RelNotes.lua
+++ RelNotes.lua
@@ -16,6 +16,9 @@ can play a sound if you enter a non PVP instance with Growl active
 alerts can be disabled in PVP
 ]])
 self:HF_Paragraph('Release Notes')
+self:RelNotes(6,15,0,[[
+FIx: PetCare/PetCare.lua
+]])
 self:RelNotes(6,14,0,[[
 Toc: 12.0.1
 ]])
